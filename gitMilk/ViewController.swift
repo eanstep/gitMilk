790f43b307605290bea95c78dca1a4aa53911e6d
eanstep <eanstep@gmail.com>
2021-11-08 20:46:06 +0200

    Initial Commit

---
+++ gitMilk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitMilk
+//
+//  Created by eanstep on 08.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
