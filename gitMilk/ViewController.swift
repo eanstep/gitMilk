e05293728c2cb4ae5c2d2e474b28cc6be5c963bd
eanstep <eanstep@gmail.com>
2021-11-09 09:17:54 +0200

    Add function didReceiveMemoryWarning and function viewWillAppear.

--- gitMilk/ViewController.swift
+++ gitMilk/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+            //Dispose of any resources that can be recreated
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
 
 
 }
